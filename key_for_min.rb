458038ef247a7217ea52aa80c40e0440c1a3e4c7
Carla Holcomb <carla.programmer@gmail.com>
2019-10-14 02:52:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,13 @@ def key_for_min_value(name_hash)
   value2 = 501
   name_hash.each do |key, value|
 
-    #while value <501
     if value < value2
       nkey = key
       value2 = value
     else
       nkey = nil
   end
-#end
+
 end
   return nkey
 
